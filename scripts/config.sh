44a0685dfee8919fd2658bfe292c1f4fd1ebe0f1
capinemo <capitan__nemo@mail.ru>
2019-04-24 16:07:42 +0300

    Created tag example v0.0.1

---
+++ scripts/config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+REPO='https://github.com/capinemo/LibraryDeploy'
+USER='capinemo'
+PASS=$GITHUB_API_KEY
+EMAIL='capitan__nemo@mail.ru'
+
+DEPLOY_BRANCH='master'
+DEPLOY_COMMAND='Build: '
+VERSION_MATCH='v\d+\.\d+\.\d+';
+
